b11c21f376167d3cc6aff9ee134bbe60eeba88d3
alozie winnie <winniechisom@gmail.com>
2023-04-04 09:42:02 -0700

    done

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string(values=[]):
+    values.append('Holberton')
+    return ', '.join(values)
+
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class LockedClass(object):
+    __slots__ = 'first_name'
+
