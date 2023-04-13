dc6fbc6d9fbcbfdc6679ee1744c39587465abbee
alozie winnie <winniechisom@gmail.com>
2023-04-13 15:00:22 -0700

    good

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+"""Contains of a single function"""
+
+
 def is_kind_of_class(obj, a_class):
     """
-    Checks if `obj` is the same class or inherit from `a_class`
-    Args:
-        obj (any): The object to compare
-        a_class (any): The class to compare with the object
-    Returns:
-        `True` if the object is an instance or inherit from the
-        specified class; otherwise `False`
+        Check if the object is an instance of a class
+        inherited from
     """
-
     return isinstance(obj, a_class)
-
